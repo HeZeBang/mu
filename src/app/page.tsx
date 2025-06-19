66cdf55d6d5ab3f16d16ca12d27b3e9c73ffb895
zambar <zambar@shanghaitech.edu.cn>
2025-06-20 00:19:22 +0800

    feat: init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,155 @@
 import Image from "next/image";
+import mdata from "./music_data_20250613.json";
+import ddata from "./chart_stats.json";
+import { Badge } from "@/components/ui/badge";
+
+interface ChartStat {
+  cnt: number;
+  diff: string;
+  fit_diff: number;
+  avg: number;
+  avg_dx: number;
+  std_dev: number;
+  dist: number[];
+  fc_dist: number[];
+}
+
+interface ChartStats {
+  charts: { [musicId: string]: ChartStat[] };
+}
+interface BasicInfo {
+  artist: string;
+  bpm: number;
+  from: string;
+  genre: string;
+  image_url: string;
+  is_new: boolean;
+  title: string;
+  version: string;
+}
+
+interface Chart {
+  charter: string;
+  notes: number[]; // [tap, hold, slide, break]
+}
+
+interface MusicData {
+  basic_info: BasicInfo;
+  charts: Chart[];
+  ds: number[]; // difficulty scores
+  id: string;
+  level: string[];
+  old_ds: number[]; // old difficulty scores
+  title: string;
+  type: string;
+}
+
+function paginate<T>(data: T[], page: number, pageSize: number = 10): T[] {
+  const start = (page - 1) * pageSize;
+  return data.slice(start, start + pageSize);
+}
+
+const difficultyNames = [
+  "Basic",
+  "Advanced",
+  "Expert",
+  "Master",
+  "Re:Master",
+];
+
+const difficultyColors = [
+  ["bg-green-500", "text-white"],
+  ["bg-yellow-500", "text-white"],
+  ["bg-red-500", "text-white"],
+  ["bg-purple-500", "text-white"],
+  ["bg-white", "text-purple-500"],
+];
 
 export default function Home() {
+  const chartStats = (ddata as ChartStats).charts;
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-2 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+          斗技杯 2025 团队挑战赛
+        </h1>
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+          曲库浏览
+        </h2>
+
+        {
+          paginate(mdata, 1, 10).map((music: MusicData) => {
+            return (
+              <div key={music.id} className="flex flex-col gap-4">
+                <div className="flex items-center gap-4">
+                  <img
+                    src={`https://maimaidx.jp/maimai-mobile/img/Music/${music.basic_info.image_url}`}
+                    alt={`${music.basic_info.title} cover`}
+                    width={100}
+                    height={100}
+                    className="rounded-lg"
+                  />
+                  <div className="flex flex-col gap-2">
+                    <div className="flex gap-2 items-start">
+                      <h3 className="text-xl font-bold">{music.basic_info.title}</h3>
+                      <code className="text-sm text-gray-500">#{music.id}</code>
+                    </div>
+                    <p className="text-sm text-gray-500">
+                      {music.basic_info.artist}
+                    </p>
+                    <div className="flex gap-2">
+                      <Badge variant="outline">分类：{music.basic_info.genre}</Badge>
+                      <Badge variant="outline">类型：{music.type}</Badge>
+                      <Badge variant="outline">版本：{music.basic_info.from}</Badge>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-wrap gap-2">
+                  {music.charts.map((chart, index) => (
+                    <div key={index} className={`flex flex-col overflow-hidden gap-1 rounded-md shadow-md border-2`}>
+                      <span className={`px-2 py-1 bg-gray-200 ${difficultyColors[index][0]} ${difficultyColors[index][1]}`}>
+                        <b>{music.level[index]}</b> ({music.ds[index].toFixed(1)}) <small>拟合: {chartStats[music.id][index].fit_diff.toFixed(2)}</small>
+                        <br />
+                        <b>{difficultyNames[index]}</b>
+                      </span>
+                      <div className="p-2">
+
+                        <div className="flex flex-col gap-1 pb-2">
+                          {music.ds[index] < 13 &&
+                            <Badge className="bg-red-500 text-white">小歌</Badge>}
+                          {["舞萌DX 2023", "舞萌DX 2024", "舞萌DX 2025"].includes(music.basic_info.from) && index == 3 &&
+                            <Badge className="bg-blue-500 text-white">新歌</Badge>}
+                          {["maimai", "maimai PLUS", "maimai GreeN", "maimai GreeN PLUS"].includes(music.basic_info.from) && index == 3 &&
+                            <Badge className="bg-cyan-500 text-white">真超檄</Badge>}
+                          {(chart.notes.at(chart.notes.length - 1) || 0) > 40 &&
+                            <Badge className="bg-yellow-500 text-white">绝赞</Badge>}
+                          {(chartStats[music.id][index].fit_diff - music.ds[index] > 0.2) && music.ds[index] >= 12 && music.ds[index] < 13 &&
+                            <Badge>地雷</Badge>}
+                          {false && <Badge className="bg-pink-500 text-white">宴会</Badge>}
+                          {index == 4 && <Badge variant="secondary">白谱</Badge>}
+                          {chart.notes.reduce((acc, curr) => acc + curr, 0) > 1000 &&
+                            <Badge className="bg-orange-500, text-white">猩猩</Badge>}
+                          {music.ds[index] > 14.5 &&
+                            <Badge className="bg-blue-500 text-white">对决</Badge>}
+                        </div>
+                        <span>
+                          谱师：{chart.charter}
+                          <br />
+                          物量：{chart.notes.reduce((acc, curr) => acc + curr, 0)}
+                          <br />
+                          音符：{chart.notes.join("/")}
+                        </span>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )
+          })
+        }
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
       </footer>
     </div>
   );
